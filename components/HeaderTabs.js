27e00fd6f3cb705a5ed80d4fbbf14796a8fe7090
kamilnr10 <kamil.nr10@gmail.com>
2022-08-09 20:52:21 +0200

    feat: add header tabs

---
+++ components/HeaderTabs.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+const HeaderTabs = () => {
+  const [activeTab, setActiveTab] = useState("Delievery");
+
+  return (
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton
+        text="Delievery"
+        btnColor="black"
+        textColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        btnColor="white"
+        textColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+    </View>
+  );
+};
+
+const HeaderButton = (props) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: props.activeTab === props.text ? "black" : "white",
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
+      }}
+      onPress={() => props.setActiveTab(props.text)}
+    >
+      <Text
+        style={{
+          color: props.activeTab === props.text ? "white" : "black",
+          fontSize: 15,
+          fontWeight: "900",
+        }}
+      >
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default HeaderTabs;
